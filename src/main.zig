e38e49801b1e68e228c6ea90c93bf42535773823
Pavitra Golchha <hello@pavi2410.com>
2026-04-03 10:38:53 +0530

    refactor 2

--- src/main.zig
+++ src/main.zig
@@ -79,49 +79,6 @@ fn isSeparator(c: u8) bool {
     return std.ascii.isWhitespace(c) or c == 0 or std.mem.indexOfScalar(u8, ",.()+-/*=~%<>[];", c) != null;
 }
 
-fn disableRawMode(original_termios: std.posix.termios) !void {
-    try std.posix.tcsetattr(std.posix.STDIN_FILENO, .FLUSH, original_termios);
-}
-
-fn enableRawMode() !std.posix.termios {
-    // step1: get the current terminal attributes
-    const original_termios = try std.posix.tcgetattr(std.posix.STDIN_FILENO);
-    var raw_termios = original_termios;
-
-    // step2: modify the attributes to enable raw mode
-    raw_termios.iflag.BRKINT = false;
-    raw_termios.iflag.ICRNL = false;
-    raw_termios.iflag.INPCK = false;
-    raw_termios.iflag.ISTRIP = false;
-    raw_termios.iflag.IXON = false;
-    raw_termios.oflag.OPOST = false;
-    raw_termios.cflag.CSIZE = .CS8;
-    raw_termios.lflag.ECHO = false;
-    raw_termios.lflag.ICANON = false;
-    raw_termios.lflag.IEXTEN = false;
-    raw_termios.lflag.ISIG = false;
-    raw_termios.cc[@intFromEnum(std.posix.V.MIN)] = 0;
-    raw_termios.cc[@intFromEnum(std.posix.V.TIME)] = 1;
-
-    // step3: set the modified attributes
-    try std.posix.tcsetattr(std.posix.STDIN_FILENO, .FLUSH, raw_termios);
-    return original_termios;
-}
-
-fn ctrlKey(k: u8) u8 {
-    return k & 0x1f;
-}
-
-fn editorReadByte() !?u8 {
-    var stdin = std.fs.File.stdin();
-    var c: [1]u8 = undefined;
-    const bytes_read = try stdin.read(&c);
-    if (bytes_read == 0) {
-        return null;
-    }
-    return c[0];
-}
-
 const EditorKey = union(enum) {
     byte: u8,
     enter,
@@ -136,126 +93,156 @@ const EditorKey = union(enum) {
     delete,
 };
 
-fn editorReadKey() !?EditorKey {
-    const first = (try editorReadByte()) orelse return null;
-    if (first == '\r') {
-        return .enter;
+const Terminal = struct {
+    original_termios: std.posix.termios,
+
+    fn init() !Terminal {
+        const original_termios = try std.posix.tcgetattr(std.posix.STDIN_FILENO);
+        var raw = original_termios;
+        raw.iflag.BRKINT = false;
+        raw.iflag.ICRNL = false;
+        raw.iflag.INPCK = false;
+        raw.iflag.ISTRIP = false;
+        raw.iflag.IXON = false;
+        raw.oflag.OPOST = false;
+        raw.cflag.CSIZE = .CS8;
+        raw.lflag.ECHO = false;
+        raw.lflag.ICANON = false;
+        raw.lflag.IEXTEN = false;
+        raw.lflag.ISIG = false;
+        raw.cc[@intFromEnum(std.posix.V.MIN)] = 0;
+        raw.cc[@intFromEnum(std.posix.V.TIME)] = 1;
+        try std.posix.tcsetattr(std.posix.STDIN_FILENO, .FLUSH, raw);
+        return .{ .original_termios = original_termios };
     }
-    if (first != '\x1b') {
+
+    fn deinit(self: Terminal) void {
+        std.posix.tcsetattr(std.posix.STDIN_FILENO, .FLUSH, self.original_termios) catch {
+            std.log.err("Failed to disable raw mode.", .{});
+        };
+    }
+
+    fn readByte() !?u8 {
+        var stdin = std.fs.File.stdin();
+        var c: [1]u8 = undefined;
+        const bytes_read = try stdin.read(&c);
+        if (bytes_read == 0) return null;
+        return c[0];
+    }
+
+    fn readKey() !?EditorKey {
+        const first = (try readByte()) orelse return null;
+        if (first == '\r') return .enter;
+        if (first != '\x1b') return .{ .byte = first };
+
+        const second = (try readByte()) orelse return .{ .byte = first };
+        const third = (try readByte()) orelse return .{ .byte = first };
+
+        if (second == '[') {
+            if (third >= '0' and third <= '9') {
+                const fourth = (try readByte()) orelse return .{ .byte = first };
+                if (fourth == '~') {
+                    return switch (third) {
+                        '3' => .delete,
+                        '5' => .page_up,
+                        '6' => .page_down,
+                        '1', '7' => .home,
+                        '4', '8' => .end,
+                        else => .{ .byte = first },
+                    };
+                }
+            }
+            return switch (third) {
+                'A' => .arrow_up,
+                'B' => .arrow_down,
+                'C' => .arrow_right,
+                'D' => .arrow_left,
+                'H' => .home,
+                'F' => .end,
+                else => .{ .byte = first },
+            };
+        }
+
+        if (second == 'O') {
+            return switch (third) {
+                'H' => .home,
+                'F' => .end,
+                else => .{ .byte = first },
+            };
+        }
+
         return .{ .byte = first };
     }
 
-    const second = (try editorReadByte()) orelse return .{ .byte = first };
-    const third = (try editorReadByte()) orelse return .{ .byte = first };
+    fn getCursorPosition() !std.posix.winsize {
+        _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[6n") catch {
+            std.log.err("Failed to write escape sequence to stdout.", .{});
+            return error.WriteFailed;
+        };
 
-    if (second == '[') {
-        if (third >= '0' and third <= '9') {
-            const fourth = (try editorReadByte()) orelse return .{ .byte = first };
-            if (fourth == '~') {
-                return switch (third) {
-                    '3' => .delete,
-                    '5' => .page_up,
-                    '6' => .page_down,
-                    '1', '7' => .home,
-                    '4', '8' => .end,
-                    else => .{ .byte = first },
-                };
-            }
+        var buf: [32]u8 = undefined;
+        var i: usize = 0;
+        while (i < buf.len - 1) {
+            const c = (try readByte()) orelse continue;
+            if (c == 'R') break;
+            buf[i] = c;
+            i += 1;
         }
+        buf[i] = 0;
 
-        return switch (third) {
-            'A' => .arrow_up,
-            'B' => .arrow_down,
-            'C' => .arrow_right,
-            'D' => .arrow_left,
-            'H' => .home,
-            'F' => .end,
-            else => .{ .byte = first },
-        };
-    }
+        if (buf[0] != '\x1b' or buf[1] != '[') return error.UnexpectedResponse;
 
-    if (second == 'O') {
-        return switch (third) {
-            'H' => .home,
-            'F' => .end,
-            else => .{ .byte = first },
-        };
-    }
-
-    return .{ .byte = first };
-}
-
-fn getCursorPosition() !std.posix.winsize {
-    _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[6n") catch {
-        std.log.err("Failed to write escape sequence to stdout.", .{});
-        return error.WriteFailed;
-    };
-
-    var buf: [32]u8 = undefined;
-    var i: usize = 0;
-    while (i < buf.len - 1) {
-        const c = (try editorReadByte()) orelse continue;
-        if (c == 'R') break;
-        buf[i] = c;
-        i += 1;
-    }
-    buf[i] = 0; // Null-terminate the buffer
-
-    if (buf[0] != '\x1b' or buf[1] != '[') return error.UnexpectedResponse;
-
-    var rows: usize = 0;
-    var cols: usize = 0;
-    var num_buf: [16]u8 = undefined;
-    var num_index: usize = 0;
-    for (buf[2..i]) |c| {
-        if (c == ';') {
-            rows = std.fmt.parseInt(usize, num_buf[0..num_index], 10) catch return error.UnexpectedResponse;
-            num_index = 0;
-        } else {
-            if (num_index < num_buf.len) {
-                num_buf[num_index] = c;
-                num_index += 1;
+        var rows: usize = 0;
+        var cols: usize = 0;
+        var num_buf: [16]u8 = undefined;
+        var num_index: usize = 0;
+        for (buf[2..i]) |c| {
+            if (c == ';') {
+                rows = std.fmt.parseInt(usize, num_buf[0..num_index], 10) catch return error.UnexpectedResponse;
+                num_index = 0;
             } else {
-                return error.UnexpectedResponse;
+                if (num_index < num_buf.len) {
+                    num_buf[num_index] = c;
+                    num_index += 1;
+                } else {
+                    return error.UnexpectedResponse;
+                }
             }
         }
-    }
-    if (num_index > 0) {
-        cols = std.fmt.parseInt(usize, num_buf[0..num_index], 10) catch return error.UnexpectedResponse;
-    } else {
-        return error.UnexpectedResponse;
+        if (num_index > 0) {
+            cols = std.fmt.parseInt(usize, num_buf[0..num_index], 10) catch return error.UnexpectedResponse;
+        } else {
+            return error.UnexpectedResponse;
+        }
+
+        return .{ .row = @intCast(rows), .col = @intCast(cols), .xpixel = 0, .ypixel = 0 };
     }
 
-    return .{
-        .row = @intCast(rows),
-        .col = @intCast(cols),
-        .xpixel = 0,
-        .ypixel = 0,
-    };
-}
-
-fn getWindowSize() !std.posix.winsize {
-    var ws: std.posix.winsize = undefined;
-    switch (std.posix.errno(std.posix.system.ioctl(
-        std.posix.STDOUT_FILENO,
-        std.posix.T.IOCGWINSZ,
-        &ws,
-    ))) {
-        .SUCCESS => return ws,
-        else => |err| {
-            // if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-            _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[999C\x1b[999B") catch {
-                std.log.err("Failed to write escape sequence to stdout.", .{});
-                return std.posix.unexpectedErrno(err);
-            };
-            return getCursorPosition() catch {
-                std.log.err("Failed to get cursor position.", .{});
-                return std.posix.unexpectedErrno(err);
-            };
-        },
+    fn getWindowSize() !std.posix.winsize {
+        var ws: std.posix.winsize = undefined;
+        switch (std.posix.errno(std.posix.system.ioctl(
+            std.posix.STDOUT_FILENO,
+            std.posix.T.IOCGWINSZ,
+            &ws,
+        ))) {
+            .SUCCESS => return ws,
+            else => |err| {
+                _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[999C\x1b[999B") catch {
+                    std.log.err("Failed to write escape sequence to stdout.", .{});
+                    return std.posix.unexpectedErrno(err);
+                };
+                return getCursorPosition() catch {
+                    std.log.err("Failed to get cursor position.", .{});
+                    return std.posix.unexpectedErrno(err);
+                };
+            },
+        }
     }
-}
 
+    fn ctrlKey(k: u8) u8 {
+        return k & 0x1f;
+    }
+};
 
 const EditorRow = struct {
     chars: []const u8,
@@ -293,7 +280,7 @@ const Editor = struct {
     rows: std.ArrayList(EditorRow),
 
     fn init(allocator: std.mem.Allocator) !Editor {
-        const ws = try getWindowSize();
+        const ws = try Terminal.getWindowSize();
         return .{
             .allocator = allocator,
             .screenRows = ws.row - 2,
@@ -620,7 +607,7 @@ const Editor = struct {
             try self.setStatusMessage(prompt_text);
             try self.refreshScreen();
 
-            const key = (try editorReadKey()) orelse continue;
+            const key = (try Terminal.readKey()) orelse continue;
             var should_cancel = false;
             var should_submit = false;
             switch (key) {
@@ -629,7 +616,7 @@ const Editor = struct {
                         should_cancel = true;
                     } else if (c == '\r') {
                         continue;
-                    } else if (c == ctrlKey('h') or c == 127) {
+                    } else if (c == Terminal.ctrlKey('h') or c == 127) {
                         _ = input.pop();
                     } else if (!std.ascii.isControl(c) and c < 128) {
                         try input.append(self.allocator, c);
@@ -985,10 +972,10 @@ const Editor = struct {
     }
 
     fn processKeypress(self: *Editor) !bool {
-        const key = (try editorReadKey()) orelse return true;
+        const key = (try Terminal.readKey()) orelse return true;
         switch (key) {
             .byte => |c| switch (c) {
-                ctrlKey('q') => {
+                Terminal.ctrlKey('q') => {
                     if (self.dirty and self.quitTimes > 0) {
                         var status_buf: [128]u8 = undefined;
                         const status = try std.fmt.bufPrint(
@@ -1002,10 +989,10 @@ const Editor = struct {
                     }
                     return false;
                 },
-                ctrlKey('s') => try self.save(),
-                ctrlKey('f') => try self.find(),
-                ctrlKey('h'), 127 => try self.deleteChar(),
-                ctrlKey('l'), '\x1b' => {},
+                Terminal.ctrlKey('s') => try self.save(),
+                Terminal.ctrlKey('f') => try self.find(),
+                Terminal.ctrlKey('h'), 127 => try self.deleteChar(),
+                Terminal.ctrlKey('l'), '\x1b' => {},
                 else => try self.insertChar(c),
             },
             .arrow_left, .arrow_right, .arrow_up, .arrow_down => self.moveCursor(key),
@@ -1040,13 +1027,11 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const original_termios = enableRawMode() catch {
+    const terminal = Terminal.init() catch {
         std.log.err("Failed to enable raw mode.", .{});
         return;
     };
-    defer disableRawMode(original_termios) catch {
-        std.log.err("Failed to disable raw mode.", .{});
-    };
+    defer terminal.deinit();
 
     var args = try std.process.argsWithAllocator(allocator);
     defer args.deinit();
